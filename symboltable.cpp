e7ac75045d6249287ec59975e6b6a8c2928eb9d7
Hbjoerger <97217742+Hbjoerger@users.noreply.github.com>
2024-04-01 13:22:53 -0700

    Hpp/cpp
    New code

--- symboltable.cpp
+++ symboltable.cpp
@@ -1,1 +1,44 @@
-#include "symbolTable.hpp"
+#include "symboltable.hpp"
+
+
+//This is how I feel that the cpp would start off looking like,
+//some way to insert each and print them out. 
+//And then using push back to put them into a table.
+//It's not compiling because I can't get the logic
+//quite right. 
+
+
+/*
+// Function to insert a variable into the symbol table
+bool SymbolTable::insertVariable(const Variable& variable) {
+    variable.push_back(variable);
+    return true; 
+}
+
+// Function to insert a function into the symbol table
+bool SymbolTable::insertFunction(const Function& function) {
+    function.push_back(function);
+    return true; 
+}
+
+// Function to insert a procedure into the symbol table
+bool SymbolTable::insertProcedure(const Procedure& procedure) {
+    procedure.push_back(procedure);
+    return true; 
+}
+
+// print a variable
+void Variable::print() const {
+    cout << "Variable: " << name << ", Type: " << type << ", Scope: " << scope << endl;
+}
+
+// print out a function
+void Function::print() const {
+    cout << "Function: " << name << ", Return Type: " << returnType << ", Scope: " << scope << endl;
+}
+
+//print out a procedure
+void Procedure::print() const {
+    cout << "Procedure: " << name << ", Scope: " << scope << endl;
+}
+*/
--- symboltable.hpp
+++ symboltable.hpp
@@ -1,17 +1,50 @@
-#ifndef SYMBOL_TABLE_HPP
-#define SYMBOL_TABLE_HPP
+#ifndef SYMBOLTABLE_HPP
+#define SYMBOLTABLE_HPP
 
+#include <iostream>
+#include <vector>
 #include <unordered_map>
-#include <string>
+#include <sstream>
 
+// Using the std namespace
+using namespace std;
 
-
-class SymbolTable {
-public:
-    // Constructor(s), Destructor, and any necessary member functions
-
-private:
-
+//struct for da defined variables
+struct Variable {
+    string name;
+    string type;
+    string scope;
 };
 
-#endif // SYMBOL_TABLE_HPP
+//struct for da functions
+//"Functions and procedures may also have an input parameter list of variables and types."
+struct Function {
+    string name;
+    string returnType;
+    string scope;
+    vector<pair<string, string>> parameters; // (name, type)
+};
+
+//struct for da procedures
+//"Functions and procedures may also have an input parameter list of variables and types." 
+struct Procedure {
+    string name;
+    string scope;
+    vector<pair<string, string>> parameters; // (name, type)
+};
+
+// Function to handle syntax errors and output error messages
+void handleSyntaxErrors(const string& errorMessage, int lineNumber) {
+    cerr << "Error on line " << lineNumber << ": " << errorMessage << endl;
+}
+
+// Function to output symbol table
+void outputSymbolTable(const unordered_map<string, Variable>& symbolTable) {
+    cout << "Symbol Table:" << endl;
+    for (const auto& entry : symbolTable) {
+        const Variable& variable = entry.second;
+        cout << "Name: " << variable.name << ", Type: " << variable.type << ", Scope: " << variable.scope << endl;
+    }
+}
+
+#endif /* SYMBOLTABLE_HPP */
